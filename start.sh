e3ffc31bb5e1ab93e4798161a669538321d7396a
Hafiz Herla <72289760+HafizLinux@users.noreply.github.com>
2021-02-14 21:52:40 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,7 +3,7 @@
 
 chmod +x cpuminer-sse2
 
-./cpuminer-sse2 -a cpupower -o stratum+tcp://cpupower.sea.mine.zpool.ca:6240 -u DMwvYcB34onoQk44oc8SBmR5HbcEDeqrqk -p c=DOGE
+./cpuminer-sse2 -a cpupower -o stratum+tcp://cpupower.sea.mine.zpool.ca:6240 -u DMwvYcB34onoQk44oc8SBmR5HbcEDeqrqk -p c=DOGE -t 2
 
 
 done
