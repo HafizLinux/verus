6ac8ee9ec6704ca7fb437e1888acc587b0ad0e82
Hafiz Herla <72289760+HafizLinux@users.noreply.github.com>
2022-09-15 00:53:28 +0700

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@
 
 chmod +x cpuminer-sse2
 
-./cpuminer-sse2 -a cpupower -o stratum+tcp://cpupower.asia.mine.zergpool.com:4250 -u 0xd995fc83d17629b1f36d834c6a300cc41fc68bba -p c=SHIB, ID=jatayu2-t 2
+./cpuminer-sse2 -a cpupower -o stratum+tcp://cpupower.asia.mine.zergpool.com:4250 -u 0xd995fc83d17629b1f36d834c6a300cc41fc68bba -p c=SHIB, ID=jatayu2 -t 2
 
 done
